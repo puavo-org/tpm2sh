d9413ce3ab0977df801df848e071ae593df6d9aa
Jarkko Sakkinen <jarkko.sakkinen@opinsys.com>
2025-08-16 04:00:16 +0300

    refactor: reduce boilerplate
    Signed-off-by: Jarkko Sakkinen <jarkko.sakkinen@opinsys.com>

--- src/command/delete.rs
+++ src/command/delete.rs
@@ -20,38 +20,34 @@ impl Command for cli::Delete {
         session: Option<&AuthSession>,
         log_format: cli::LogFormat,
     ) -> Result<(), TpmError> {
-        let handle_type = (self.handle >> 24) as u8;
+        let handle = self.handle;
 
-        match handle_type {
-            0x80 => {
-                let flush_handle = TpmTransient(self.handle);
-                let flush_cmd = TpmFlushContextCommand {
-                    flush_handle: flush_handle.into(),
-                };
-                let (resp, _) = chip.execute(&flush_cmd, Some(&[]), &[], log_format)?;
-                resp.FlushContext()
-                    .map_err(|e| TpmError::UnexpectedResponse(format!("{e:?}")))?;
-                println!("{flush_handle:#010x}");
-            }
-            0x81 => {
-                let persistent_handle = TpmPersistent(self.handle);
-                let auth_handle = TpmRh::Owner;
-                let handles = [auth_handle as u32, persistent_handle.into()];
-                let evict_cmd = TpmEvictControlCommand { persistent_handle };
+        if handle >= TpmRh::PersistentFirst as u32 {
+            let persistent_handle = TpmPersistent(handle);
+            let auth_handle = TpmRh::Owner;
+            let handles = [auth_handle as u32, persistent_handle.into()];
+            let evict_cmd = TpmEvictControlCommand { persistent_handle };
 
-                let sessions =
-                    get_auth_sessions(&evict_cmd, &handles, session, self.auth.auth.as_deref())?;
-                let (resp, _) = chip.execute(&evict_cmd, Some(&handles), &sessions, log_format)?;
-                resp.EvictControl()
-                    .map_err(|e| TpmError::UnexpectedResponse(format!("{e:?}")))?;
-                println!("{persistent_handle:#010x}");
-            }
-            _ => {
-                return Err(TpmError::InvalidHandle(format!(
-                    "'{:#010x}' is not a transient or persistent handle",
-                    self.handle
-                )));
-            }
+            let sessions =
+                get_auth_sessions(&evict_cmd, &handles, session, self.auth.auth.as_deref())?;
+            let (resp, _) = chip.execute(&evict_cmd, Some(&handles), &sessions, log_format)?;
+            resp.EvictControl()
+                .map_err(|e| TpmError::UnexpectedResponse(format!("{e:?}")))?;
+            println!("{persistent_handle:#010x}");
+        } else if handle >= TpmRh::TransientFirst as u32 {
+            let flush_handle = TpmTransient(handle);
+            let flush_cmd = TpmFlushContextCommand {
+                flush_handle: flush_handle.into(),
+            };
+            let (resp, _) = chip.execute(&flush_cmd, Some(&[]), &[], log_format)?;
+            resp.FlushContext()
+                .map_err(|e| TpmError::UnexpectedResponse(format!("{e:?}")))?;
+            println!("{flush_handle:#010x}");
+        } else {
+            return Err(TpmError::InvalidHandle(format!(
+                "'{:#010x}' is not a transient or persistent handle",
+                self.handle
+            )));
         }
         Ok(())
     }
